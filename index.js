798046bbda684db1283960071beaf3a25536142d
Utkarshgit19 <utkarsh.nandankar.civ22@itbhu.ac.in>
2025-08-13 23:09:47 +0530

    created neon db

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Required for Neon/Render Postgres
+    }
 });
 
 
